d3c2b9b58919f64b500b3baa615fe6fe78b55d1f
Aleksandr <sssalex06@gmail.com>
2021-11-06 14:47:43 +0200

    fix bug

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ class Todo {
       <label for="item-${this.id}"><input class="todo__complited" data-id-check="${this.id}" type="checkbox" id="item-${this.id}" ${flag2}> <span>${flag1}</span></label>
       <div class="todo__text-wrapper">
         <p class="todo__text" id="${this.userId}">${this.title}</p>
-        <a class="todo__link" data-id-link="${this.id}">details...</a>
+        <a href="/detail.html" target="_blank" class="todo__link" data-id-link="${this.id}">details...</a>
       </div>
       <button class="todo__delete" data-id-del="${this.id}">delete</button>
     </div> `
@@ -36,7 +36,7 @@ function renderItemTodo(e) {
   let obj = Object.assign(findItemTodo(e, 'data-id-link'))//console.log(obj);
   let itemString = JSON.stringify(obj)
   localStorage.setItem('todo-item', itemString) 
-  window.location.href = './detail.html';
+  // window.location.href = './detail.html';
 }
 // 
 function findItemTodo(e, attr) {
